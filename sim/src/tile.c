6e23f5385e4486f9b06ed27e108c2564c65779e6
Sohil Shah <sohils@cmu.edu>
2015-09-24 13:44:28 -0400

    [tile/neurons] began writing tile/neuron/vector files and associated headers

--- sim/src/tile.c
+++ sim/src/tile.c
@@ -1,8 +1,66 @@
 /*
  * Author: Sohil Shah
  *
- * Description: This is the header file for tile.c. Contains definitions of 
- * structures and constants relevant to the tiles. 
+ * Description: Implements the tile unit of the neural net as well as 
+ * its associated neurons
  *
  */
 
+#include "tile.h"
+#include "err_wrappers.h"
+
+
+/**
+ * Constructor for a tile: allocates space for tile, sets parameters, and 
+ * creates neurons associated with tile. 
+ */
+tile_t Tile(uint32_t num_neurons, uint32_t num_inputs, uint32_t num_outputs, 
+	    uint32_t (*activation_fn)(uint32_t)) {
+  tile_t tile = Malloc(sizeof(struct tile));
+
+  tile->num_neurons = num_neurons;
+  tile->num_inputs = num_inputs;
+  tile->neurons = Malloc(num_neurons * sizeof(neuron_t));
+
+  for (int i = 0; i < num_neurons; ++i) {
+    neuron_t neuron = Neuron(num_inputs, activation_fn);    
+    tile->neurons[i] = neuron;
+  }
+
+  return tile;
+}
+
+/**
+ * Destructor for a tile: destroys neurons accociated with tile and frees
+ * data structures allocated to tile. 
+ */
+void tile_destroy(tile_t tile) {
+  for (int i = 0; i < tile->num_neurons; ++i) {
+    neuron_destroy(tile->neurons[i]);
+  }
+
+  Free(tile->neurons);
+  Free(tile);
+}
+
+/**
+ * Constructor for a neuron: allocates space for the neuron and sets parameters
+ * relevent to the neuron. 
+ */
+neuron_t Neuron(uint32_t input_len, uint32_t (*activation_fn)(uint32_t)) {
+  neuron_t neuron = Malloc(sizeof(struct neuron));
+
+  neuron->input_len = input_len;
+  neuron->activation_fn = activation_fn;
+}
+
+/**
+ * Computes the output of a neuron given its activation function by performing a 
+ * vector dot product of the input and weights. This is then summed and 
+ * passed through the activation function. 
+ */
+uint32_t evaluate_neuron(neuron_t neuron, vector_t input, vector_t weights);
+
+void neuron_destroy(neuron_t neuron) {
+  Free(neuron);
+}
---
+++ sim/src/vector.c
@@ -0,0 +1,23 @@
+/*
+ * Author: Sohil Shah
+ *
+ * Description: implements a vector type used to pass inputs and
+ * outputs between neurons and tiles. 
+ *
+ */
+
+
+vector_t Vector(size_t length, size_t elem_size) {
+
+}
+
+vector_t vmult(vector_t v1, vector_t v2) {
+
+}
+
+vector_t vadd(vector_t v1, vector_t v2) {
+
+}
+void vector_destroy(vector_t vector) {
+
+}
