43ac7b617b0f1b36b1e9b9795985efed6f92b42e
Dongjoon Park <dongjoop@ece002.campus.ece.cmu.local>
2015-09-25 17:11:44 -0400

    [sim] weights.c, weights.h added

---
+++ sim/src/weights.c
@@ -0,0 +1,34 @@
+/*
+ * Author: DJ Park
+ *
+ * Description: Implements a function that initialize 3-d weight array.
+ */
+#include "weights.h"
+#include "err_wrappers.c"
+
+/**
+ * NOTE: Remember to free returned array.
+ */
+int*** initWeights (int numLayers, ... ){
+    va_list arguments;
+    int i,j,k,numNeurons_prevLayer, numNeurons_currLayer;
+
+    va_start ( arguments, numLayers);
+    int *** array = (int***) Malloc(sizeof(int**)*(numLayers-1));
+    for (i=0; i<numLayers; i++){
+        numNeurons_currLayer = va_arg (arguments, int);
+        if(i!=0){ // there are total numLayers-1 2-d array
+            array[i-1] = (int**) Malloc(sizeof(int*)*numNeurons_prevLayer);
+            for(j=0; j<numNeurons_prevLayer; j++){
+               array[i-1][j] = (int*) Malloc(sizeof(int)*numNeurons_currLayer);
+               for(k=0; k<numNeurons_currLayer; k++){
+                   array[i-1][j][k] = 1; // initialize as 1
+               }
+            }
+        }
+        numNeurons_prevLayer = numNeurons_currLayer;
+    }
+    va_end (arguments);
+
+    return array;
+}
---
+++ sim/src/weights.h
@@ -0,0 +1,34 @@
+/*
+ * Author: DJ Park
+ *
+ * Description: This is the header file for weights.c.
+ *
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Initialize 3-d weight array when given the number of layers and
+ * the number of neurons for each layer. The first argument is the
+ * number of layers, and the rest of the arguments are the number of 
+ * neurons for each layer. For instance, when there are
+ * three layers and each has 2,4,3 neurons respectively, the command
+ * for this function would be
+ * int*** weightsArray = initWeights(3,2,4,3).
+ *
+ * weightsArray[1][0][2] accesses the 1st layer's 0th neuron's weight 
+ * value to next layer's 2nd neuron.
+ *
+ * 0th layer to 1st layer, 2x4 array
+ * [ - - - -   
+ *   - - - - ] 
+ * 1st layer to 2nd layer, 4x3 array
+ * [ - - *
+ *   - - -
+ *   - - -
+ *   - - - ]
+ *
+ */
+int*** initWeights (int numLayers, ...);
