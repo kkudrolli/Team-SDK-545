4f05340c1a5d7bfd8f6e8f47b04f687c40c86ae2
Dongjoon Park <dongjoop@ece002.campus.ece.cmu.local>
2015-10-08 23:06:52 -0400

    [sim] weights.c,weights.h fixed

--- sim/src/weights.c
+++ sim/src/weights.c
@@ -7,71 +7,125 @@
 #include "weights.h"
 
 /**
- * NOTE: Remember to free returned weightfile. The number of arguments for this function
- * should be numLayers+1.
+ * NOTE: Remember to free returned weightfile.
+ *
+ * ex) initWeights(4, 100, 1024, 512, 256) when image size is 10x10
+ * 4: the number of layers + 1, think this number as the number of arguments after it.
+ * 100 : the number of pixels
  */
-weightfile_t initWeights(uint32_t numLayers, ...){
+weightfile_t initWeights(uint32_t numLayersPlusOne, ...){
 
     va_list arguments;
-    uint32_t i,j,numNeurons_prevLayer, numNeurons_currLayer;
+    uint32_t i,j,k,numNeurons_prevLayer,numNeurons_currLayer,numPixels,numLayers;
+    time_t t;
+    srand((unsigned) time(&t));
 
-    va_start (arguments, numLayers);
-    weightfile_t weightfile = (weightfile_t) Malloc(sizeof(vector_t*)*(numLayers-1));
-    for ( i=0; i < numLayers; i++ ){
-        numNeurons_currLayer = va_arg (arguments, uint32_t);
-        if(i!=0){
-            weightfile[i-1] = (vector_t*) Malloc(sizeof(vector_t)*numNeurons_currLayer);
-            for(j=0; j<numNeurons_currLayer; j++){
-                weightfile[i-1][j] = Vector(numNeurons_prevLayer);
+    va_start (arguments, numLayersPlusOne); // start receiving arguments
+    numPixels = va_arg (arguments, uint32_t); // number of pixels of the image
+    assert(numPixels>0);
+    numLayers = numLayersPlusOne-1;
+    //printf("numPixels=%d\n",numPixels);
+
+    weightfile_t weightfile = (weightfile_t) Malloc(sizeof(vector_t*)*(numLayers));
+    for (i=0 ; i<numLayers; i++){
+        numNeurons_currLayer = va_arg(arguments, uint32_t);
+        assert(numNeurons_currLayer>0);
+        //printf("numNeurons_currLayer=%d\n",numNeurons_currLayer);
+        weightfile[i] = (vector_t*) Malloc(sizeof(vector_t)*numNeurons_currLayer);
+        for(j=0 ; j<numNeurons_currLayer; j++){
+            if(i==0){ // if it's 0th layer
+                weightfile[i][j] = Vector(numPixels);
+                for(k = 0; k < numPixels; k++){
+                    weightfile[i][j]->data[k] = rand()%50; // random value between 0,50
+                }
+            }
+            else{
+                weightfile[i][j] = Vector(numNeurons_prevLayer);
+                for(k = 0; k < numNeurons_prevLayer; k++){
+                    weightfile[i][j]->data[k] = rand()%50; // random value between 0,50
+                }
             }
         }
         numNeurons_prevLayer = numNeurons_currLayer;
     }
-    va_end (arguments);
+
+    va_end (arguments); // end receiving arguments
     return weightfile;
 }
 
 /**
- * REQUIRES layer > 0
+ * Returns weight vector for dest_neuron.
  */
 vector_t getWeights(weightfile_t weightfile, uint32_t layer, uint32_t dest_neuron){
-    assert(layer>0);
-
-    return weightfile[layer-1][dest_neuron];
+    return weightfile[layer][dest_neuron];
 }
 
 /**
- * REQUIRES layer > 0
+ * Sets weights for dest_neuron.
  */
 void setWeights(weightfile_t weightfile, uint32_t layer, uint32_t dest_neuron, vector_t weights){
-    assert(layer>0);
-    assert(sizeof(weightfile[layer-1][dest_neuron])==sizeof(weights));
+    assert(weightfile[layer][dest_neuron]->length==weights->length);
 
     for(size_t i = 0; i < weights->length; i++){
-        weightfile[layer-1][dest_neuron]->data[i] = weights->data[i];
+        weightfile[layer][dest_neuron]->data[i] = weights->data[i];
     }
-    weightfile[layer-1][dest_neuron]->length = weights->length;
+    weightfile[layer][dest_neuron]->length = weights->length;
 }
 
 /**
- * NOTE: The number of arguments for this function should be numLayers+2 because
- * the first argument is weightfile, second one is the number of layers, and the
- * rest of them are the number of neurons for each layer.
+ * ex) freeWeightfile(weightfile, 4, 100, 1024, 512, 256) when image size is 10x10
+ * 4: the number of layers + 1, think this number as the number of arguments after it.
+ * 100: the number of pixels
  */
-void freeWeightfile(weightfile_t weightfile, uint32_t numLayers, ...){
+void freeWeightfile(weightfile_t weightfile, uint32_t numLayersPlusOne, ...){
     va_list arguments;
-    uint32_t i,j,numNeurons_currLayer;
+    uint32_t i,j,numNeurons_currLayer,numPixels,numLayers;
 
-    va_start (arguments, numLayers);
+    va_start (arguments, numLayersPlusOne);
+    numPixels = va_arg (arguments, uint32_t); // number of pixels of the image
+    assert(numPixels>0);
+    numLayers = numLayersPlusOne-1;
+    //printf("numPixels=%d, numLayers=%d\n",numPixels,numLayers);
+
     for (i=0; i < numLayers; i++){
         numNeurons_currLayer = va_arg (arguments, uint32_t);
-        if(i!=0){
-            for(j=0; j<numNeurons_currLayer; j++){
-                vector_destroy(weightfile[i-1][j]); // free neurons in current layer
-            }
-            Free(weightfile[i-1]); // free current layer
+        for(j=0; j<numNeurons_currLayer; j++){
+            vector_destroy(weightfile[i][j]); // free neurons in current layer
         }
+        Free(weightfile[i]); // free current layer
     }
     va_end (arguments);
     Free(weightfile); // free node
 }
+
+
+/**
+ * TEST
+ */
+/*
+int main(){
+    weightfile_t weightfile = initWeights(4,100,1024,512,256);
+
+    printf("getWeights(weightfile,0,0)->data[0]=%d\n",getWeights(weightfile,0,0)->data[0]);
+    printf("getWeights(weightfile,0,0)->data[99]=%d\n",getWeights(weightfile,0,0)->data[99]);
+   
+    printf("getWeights(weightfile,0,1023)->data[0]=%d\n",getWeights(weightfile,0,1023)->data[0]);
+    printf("getWeights(weightfile,2,0)->data[0]=%d\n",getWeights(weightfile,2,0)->data[0]);
+    vector_t weights = Vector(100);
+    weights->data[0] = 100;
+    weights->data[1] = 101;
+    weights->data[2] = 102;
+    setWeights(weightfile,0,1023,weights);
+    setWeights(weightfile,0,1022,weights);
+    setWeights(weightfile,0,1021,weights);
+
+    printf("getWeights(weightfile,0,1023)->data[0]=%d\n",getWeights(weightfile,0,1023)->data[0]);
+    printf("getWeights(weightfile,0,1023)->data[1]=%d\n",getWeights(weightfile,0,1023)->data[1]);
+ 
+    vector_destroy(weights);
+
+    freeWeightfile(weightfile,4,100,1024,512,256);
+   
+    return 0;
+}
+*/
--- sim/src/weights.h
+++ sim/src/weights.h
@@ -11,28 +11,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "err_wrappers.h"
 #include "vector.h"
 
 typedef vector_t** weightfile_t;
 
-
-typedef vector_t** weightfile_t;
-
-/**
- * Initializes weightfile when given the number of layers and
- * the number of neurons for each layer. The first argument is the
- * number of layers, and the rest of the arguments are the number of 
- * neurons for each layer. For instance, when there are
- * three layers and each has 2,4,3 neurons respectively, the command
- * for this function would be
- * weightfile_t weightfile = initWeights(3,2,4,3).
- *
- * getWeights(weightfile,1,0)->data accesses all the weight values to the 1st layer's 0th neuron.
- * getWeights(weightfile,1,0)->length should be same as the number of neurons in 0th layer
- *
- */
-weightfile_t initWeights(uint32_t numLayers, ...);
+weightfile_t initWeights(uint32_t numLayersPlusOne, ...);
 
 /**
  * Returns wegiht vector for dest_neuron.
@@ -44,10 +29,5 @@ vector_t getWeights(weightfile_t weightfile, uint32_t layer, uint32_t dest_neuro
  */
 void setWeights(weightfile_t weightfile, uint32_t layer, uint32_t dest_neuron, vector_t weights);
 
-/**
- * This function frees the weightfile.
- * freeWeightfile(weightfile,3,2,4,3)
- * would free the memory allocated for weightfile.
- */
-void freeWeightfile(weightfile_t weightfile, uint32_t numLayers, ...);
+void freeWeightfile(weightfile_t weightfile, uint32_t numLayersPlusOne, ...);
 #endif
