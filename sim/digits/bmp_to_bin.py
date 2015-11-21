df240847faacbad3182dc7870e1d5e143c249de4
Kais A. Kudrolli <kkudroll@ece008.ece.cmu.edu>
2015-11-21 14:27:04 -0500

    [Stylized images] Put stylized image hex strings into hex_strings.txt

--- sim/digits/bmp_to_bin.py
+++ sim/digits/bmp_to_bin.py
@@ -4,7 +4,7 @@ bmp_to_bin.py
 Author: Kais Kudrolli
 
 Description: Script to convert all bmp's to a hex string of their pixel data.
-Then write that to a file *.bin.
+Then writes all 10 into hex_strings.txt.
 """
 
 class bcolors:
@@ -21,22 +21,7 @@ FILE_MODE = "rb"
 HDR_SZ = 122
 IMG_SZ = 784
 
-def bmp_to_bin(bmp_name):
-    bmp_file = open(bmp_name, FILE_MODE);
-    header = bmp_file.read(HDR_SZ)
-    #print " ".join(hex(ord(n)) for n in header)
-
-    hex_string = ""
-    for i in xrange(0, IMG_SZ*3):
-        byte = bmp_file.read(1)
-        byte_str = format(ord(byte), 'x')
-        if (len(byte_str) == 1):
-            byte_str = "0" + byte_str
-        if (i % 3 == 0):
-            hex_string += byte_str
-    
-    #print hex_string
-    #print len(hex_string)
+def print_num(hex_string):
     for i in xrange(0, 28):
         for j in xrange(0, 28*2, 2):
             print_byte = (hex_string[(i*56)+j] + hex_string[(i*56)+(j+1)])
@@ -46,10 +31,30 @@ def bmp_to_bin(bmp_name):
                print bcolors.OKGREEN + print_byte + bcolors.ENDC,
         print "\n",
 
+def bmp_to_bin(bmp_name):
+    bmp_file = open(bmp_name, FILE_MODE);
+    header = bmp_file.read(HDR_SZ)
+
+    hex_string = ""
+    for i in xrange(0, IMG_SZ*3):
+        byte = bmp_file.read(1)
+        byte_str = format(ord(byte), 'x')
+        if (len(byte_str) == 1):
+            byte_str = "0" + byte_str
+        if (i % 3 == 0):
+            hex_string += byte_str
+
     bmp_file.close()
+    return hex_string
 
 
 if __name__ == "__main__":
+    hex_file = open("./hex_strings.txt", "w")
+
     for i in xrange(0, 10):
-        bmp_to_bin("./" + str(i) + ".bmp")
+        hex_string = bmp_to_bin("./" + str(i) + ".bmp")
+        hex_file.write("Number: " + str(i) + "\n" + hex_string + "\n")
+
+    hex_file.close() 
+
     
